5539f67e539a3ca93fd9ce73f8e1723a028be6ff
최미래 <miraechoi@dgsw.hs.kr>
2024-12-19 20:30:49 +0900

    feature: add create board service logic

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
@@ -7,5 +8,14 @@ export class BoardsService {
 
   getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const Board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC
+    }
   }
 }
