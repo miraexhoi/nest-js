d662d9156ae32393a867e5dce908a7ef6f56c442
최미래 <miraechoi@dgsw.hs.kr>
2024-12-28 12:15:24 +0900

    feature: add create board api

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,49 +1,21 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { BoardStatus } from './board-status.enum';
 import { CreateBoardDto } from './dto/create-board.dto';
-import { BoardStatusValidationPipe } from './pipes/board-status-validation-pipe';
 import { Board } from './board.entity';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
-  // @Get("/")
-  // getAllBoard(): Board[] {
-  //   return this.boardsService.getAllBoards();
-  // }
-  //
-  // @Post("/create")
-  // @UsePipes(ValidationPipe)
-  // craeteBoard(
-  //   @Body() createBoardDto: CreateBoardDto
-  // ): Board {
-  //   return this.boardsService.createBoard(createBoardDto);
-  // }
-  //
+  @Post()
+  @UsePipes(ValidationPipe)
+  createBoard(@Body() createBoardDto: CreateBoardDto) : Promise<Board> {
+    return this.boardsService.createBoard(createBoardDto);
+  }
 
   @Get("/:id")
   getBoardById(@Param('id') id: number) : Promise<Board> {
     return this.boardsService.getBoardById(id);
   }
-
-  // @Get("/:id")
-  // getBoardById(@Param('id') id: string): Board {
-  //   return this.boardsService.getBoardById(id);
-  // }
-  //
-  // @Delete("/:id")
-  // deleteBoardById(@Param('id') id: string): void {
-  //   this.boardsService.deleteBoardById(id);
-  // }
-  //
-  // @Patch("/:id/status")
-  // updateBoardById(
-  //   @Param('id') id: string,
-  //   @Body('status', BoardStatusValidationPipe) status: BoardStatus
-  // ) {
-  //   return this.boardsService.updateBoardById(id, status);
-  // }
 
 }
