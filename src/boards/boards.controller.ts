849dbe0b3842d9203bc492a73a67f6e2d1ab3616
최미래 <miraechoi@dgsw.hs.kr>
2024-12-30 23:40:21 +0900

    refactor: add update board status api

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -13,6 +13,8 @@ import {
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { Board } from './board.entity';
+import { BoardStatus } from './board-status.enum';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation-pipe';
 
 @Controller('boards')
 export class BoardsController {
@@ -32,6 +34,14 @@ export class BoardsController {
   @Delete('/:id')
   deleteBoard(@Param('id', ParseIntPipe) id: number) : Promise<void> {
     return this.boardsService.deleteBoard(id);
+  }
+
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+  ): Promise<Board> {
+    return this.boardsService.updateBoardStatus(id, status);
   }
 
 }
