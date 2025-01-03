ecf64ad42c0b44c4b9ed67f065e5941a5d8a9bc7
최미래 <miraechoi@dgsw.hs.kr>
2025-01-03 20:13:41 +0900

    auth: add catch exception to repository method

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,7 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User>{
@@ -8,6 +9,14 @@ export class UserRepository extends Repository<User>{
     const { username, password } = authCredentialDto;
     const user = this.create({ username, password });
 
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing name');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
