b7beb7cbf4826c585b872629d66fbb29c977f3fb
최미래 <miraechoi@dgsw.hs.kr>
2025-01-04 23:31:05 +0900

    auth: sign in servic logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -12,5 +13,16 @@ export class AuthService {
 
   async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
     return this.userRepository.createUser(authCredentialDto);
+  }
+
+  async SignIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+    const { username, password } = authCredentialDto;
+    const user = await this.userRepository.findOne({ username });
+
+    if(user && (await bcrypt.compare(password, user.password))) {
+      return 'logIn success'
+    } else {
+      throw new UnauthorizedException('Login Failed');
+    }
   }
 }
