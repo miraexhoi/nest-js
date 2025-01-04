9f6400af83b8b8ba90e926d0dd35640f12cd1a9f
최미래 <miraechoi@dgsw.hs.kr>
2025-01-04 23:50:19 +0900

    auth: add jwt service to auth service constructor

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
-    private userRepository: UserRepository
+    private userRepository: UserRepository,
+    private jwtService: JwtService
   ) {}
 
   async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
