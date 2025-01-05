eeb20e5377da03711db08e899961d294b0faa921
최미래 <miraechoi@dgsw.hs.kr>
2025-01-05 23:04:56 +0900

    auth: jwt strategy validate logic

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,8 +1,31 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+import { ExtractJwt } from 'passport-jwt';
+import { User } from './user.entity';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+      @InjectRepository(UserRepository)
+      private userRepository: UserRepository
+    ) {
+      super({
+        secretOrKey: 'Secret1234',
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+      })
+    }
 
+    async validate(payload){
+      const { username } = payload;
+      const user: User = await this.userRepository.findOne({ username });
+
+      if(!user) {
+        throw new UnauthorizedException();
+      }
+
+      return user;
+    }
 }
