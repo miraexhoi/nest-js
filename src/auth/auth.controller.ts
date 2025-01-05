5d197b567054bbed712b99dd4f4ebcf82a12e1d9
최미래 <miraechoi@dgsw.hs.kr>
2025-01-05 23:10:17 +0900

    auth: add auth test api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +15,11 @@ export class AuthController {
   @Post('/signin')
   signIn(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto) : Promise<{ accessToken: string }> {
     return this.authService.SignIn(authCredentialDto)
+  }
+
+  @Post('/authTest')
+  @UseGuards(AuthGuard())
+  authTest(@Req() req) {
+    console.log('req', req);
   }
 }
