becc10f20d1588fa2a3bf89fd9af0d55b5a7f5ce
최미래 <miraechoi@dgsw.hs.kr>
2025-01-19 23:35:10 +0900

    auth: imports auth module to boards module

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -3,10 +3,12 @@ import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardRepository } from './board.repository';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([BoardRepository]),
+    AuthModule
   ],
   controllers: [BoardsController],
   providers: [BoardsService]
